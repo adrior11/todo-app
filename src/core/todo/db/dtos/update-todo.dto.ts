54db866c427cc5cd65b4f36c6b0bea827c4ae67b
adrior11 <adrian.schneider11@gmail.com>
2024-10-04 18:46:01 +0200

    chore: add http-client setup for local API testing

--- src/core/todo/db/dtos/update-todo.dto.ts
+++ src/core/todo/db/dtos/update-todo.dto.ts
@@ -1,14 +1,17 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateTodoDto } from './create-todo.dto';
-import { IsBoolean, IsOptional, IsString, Max, MaxLength } from 'class-validator';
+import { IsBoolean, IsNotEmpty, IsOptional, IsString, MaxLength, MinLength } from 'class-validator';
 
 export class UpdateTodoDto extends PartialType(CreateTodoDto) {
     @IsOptional()
     @IsBoolean()
+    @IsNotEmpty()
     completed?: boolean;
 
     @IsOptional()
     @IsString()
     @MaxLength(255)
+    @MinLength(1)
+    @IsNotEmpty()
     description?: string;
 }
